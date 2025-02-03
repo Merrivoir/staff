c7569af396c888394b1740c55b960298494899f3
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2025-02-03 21:24:18 +0500

    update auth and date

--- calendar.js
+++ calendar.js
@@ -1,8 +1,12 @@
 
     const calendarInput = document.getElementById("calendar")
+
     flatpickr(calendarInput, {
-      dateFormat: "Y-m-d", // Формат даты: Год-месяц-день
-      defaultDate: new Date(), // Устанавливаем текущую дату по умолчанию
+      locale: "ru", // Понедельник первый
+      dateFormat: "d.m.Y",
+      defaultDate: new Date(),
+      enableTime: false,
+      weekNumbers: true,
       onChange: function (selectedDates, dateStr, instance) {
         console.log("Выбрана дата:", dateStr)
         fetchData(dateStr)
